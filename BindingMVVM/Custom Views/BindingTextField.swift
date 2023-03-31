32588ccf4d53ed7adf7b112093062a422fc32fb3
Daniel Braga <dbragabarbosa@gmail.com>
2023-03-31 12:49:43 -0300

    View to ViewModel Binding

---
+++ BindingMVVM/Custom Views/BindingTextField.swift
@@ -0,0 +1,44 @@
+//
+//  BindingTextField.swift
+//  BindingMVVM
+//
+//  Created by Daniel Braga Barbosa on 31/03/23.
+//
+
+import Foundation
+import UIKit
+
+class BindingTextField: UITextField
+{
+    var textChanged: (String) -> Void = { _ in }
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit()
+    {
+        addTarget(self, action: #selector(textFieldDidChanged), for: .editingChanged)
+    }
+    
+    func bind(callback: @escaping (String) -> Void)
+    {
+        textChanged = callback
+    }
+    
+    @objc func textFieldDidChanged(_ textField: UITextField)
+    {
+        if let text = textField.text
+        {
+            textChanged(text)
+        }
+    }
+}
---
+++ BindingMVVM/View Models/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  BindingMVVM
+//
+//  Created by Daniel Braga Barbosa on 31/03/23.
+//
+
+import Foundation
+
+struct LoginViewModel
+{
+    var username: String = ""
+    var password: String = ""
+}
